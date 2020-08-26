ff87025f1bb857ce2e91f11f249456b67b17f627
federico.quaglia <federico.quaglia@external.thalesgroup.com>
2020-08-27 00:56:13 +0200

    fix db name

--- include/core.inc.php
+++ include/core.inc.php
@@ -1,6 +1,6 @@
 <?php
 function connectToDb(){
-	$link = mysqli_connect("127.0.0.1", "root", "", "gestionale_sett2019");
+	$link = mysqli_connect("127.0.0.1", "root", "", "gestionale_sett2020");
 
 	if (!$link) {
 	    echo "Error: Unable to connect to MySQL." . PHP_EOL;
