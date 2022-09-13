17485462a44d7d134954789fe6dfe4d619e67bbf
Federico Quaglia <federico.quaglia@thalesgroup.com>
2022-09-14 01:35:49 +0200

    query filtro data

--- ajax/ottieni_piatti_in_produzione_in_corso.ajax.php
+++ ajax/ottieni_piatti_in_produzione_in_corso.ajax.php
@@ -1,31 +1,38 @@
 <?php
 	require_once '../include/core.inc.php';
 	$link=connectToDb();
-	$portate=array();
+	$date=ottieni_data_serata_attuale();
+    if($date <=0){
+        $comanda=array('error' => '#error#Errore durante l\'acquisizione della data');
+    }
+    else{
+		$portate=array();
 		$idprg=0;
-		$query_idprog="SELECT min(idprogrammazione) as idprg FROM programmazioneordini WHERE stato = 2 AND categoria IN ('antipasto','bruschette e crostoni','dolce')";
+		$query_idprog="SELECT min(idprogrammazione) as idprg FROM programmazioneordini WHERE stato = 2 AND categoria IN ('antipasto','bruschette e crostoni','dolce') AND serata='$date'";
 		$result_idprog = mysqli_query($link, $query_idprog) or die("#error#".mysqli_error($link));
-	    while ($row_idprg = mysqli_fetch_assoc($result_idprog)) {
-	    	$idprg=$row_idprg['idprg'];
-	    }	  
-	    if($idprg>0){
-	    	$query="SELECT *,COUNT(id) AS nr FROM programmazioneordini 
+		while ($row_idprg = mysqli_fetch_assoc($result_idprog)) {
+			$idprg=$row_idprg['idprg'];
+		}	  
+		if($idprg>0){
+			$query="SELECT *,COUNT(id) AS nr FROM programmazioneordini 
 			WHERE idprogrammazione=$idprg
 			AND stato=2 
 			AND categoria IN ('antipasto','bruschette e crostoni','dolce')
+			AND serata='$date'
 			GROUP BY portata,idprogrammazione
 			ORDER BY portata";
 
 			$result = mysqli_query($link, $query) or die("#error#".mysqli_error($link));
-		    while ($row = mysqli_fetch_assoc($result)) {
-		    	array_push($portate, array('portata' => $row['portata'], 
-		    								'indice' => $row['indice'],
-		    								'tavolo' => $row['tavolo'],
-		    								'nr' => $row['nr'],
-		    								'idprg' => $row['idprogrammazione'],
-		    								'cat' => $row['categoria']));
-		    }  
-	    }
+			while ($row = mysqli_fetch_assoc($result)) {
+				array_push($portate, array('portata' => $row['portata'], 
+											'indice' => $row['indice'],
+											'tavolo' => $row['tavolo'],
+											'nr' => $row['nr'],
+											'idprg' => $row['idprogrammazione'],
+											'cat' => $row['categoria']));
+			}  
+		}	
+	}													
 		
 	disconnetti_mysql($link, NULL); #visto che non ho un result_set gli passo NULL.. nella funzione in core.in.php ho aggiunto il controllo
 
